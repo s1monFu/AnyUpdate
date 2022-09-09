cf1147f23a19699b91124d03a9304e8fc026ad02
s1monFu <laissezfu@gmail.com>
2022-09-09 00:56:24 -0500

    better dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,29 +1,16 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Progress from './GoalTracking';
-import GoalTracking from './GoalTracking';
-import ProjectOverview from './ProjectOverview';
-import Calendar from './Calendar';
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import GoalTracking from "./GoalTracking";
+import ProjectOverview from "./ProjectOverview";
+import Calendar from "./Calendar";
+import { positions } from "@mui/system";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-
   return (
     <div
       role="tabpanel"
@@ -50,7 +37,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
   };
 }
 
@@ -62,36 +49,57 @@ function Dashboard() {
   };
 
   return (
-    <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 500 }}
-    >
-      <Tabs
-        orientation="vertical"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
+    <Box>
+      <Box>
+        <a>My Project</a>
+      </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.paper",
+          display: "flex",
+          height: "100vh",
+          borderLeft: "0",
+          left:"0"
+        }}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <ProjectOverview></ProjectOverview>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <GoalTracking></GoalTracking>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Calendar></Calendar>
-      </TabPanel>
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{
+            borderRight: 1,
+            borderColor: "divider",
+            backgroundColor: "#f7f3f6",
+          }}
+        >
+          <Tab
+            label="Project Overview"
+            sx={{ fontSize: "18px" }}
+            {...a11yProps(0)}
+          />
+          <Tab
+            label="Goal Tracking"
+            sx={{ fontSize: "18px" }}
+            {...a11yProps(1)}
+          />
+          <Tab label="Calendar" sx={{ fontSize: "18px" }} {...a11yProps(2)}  />
+          <Tab label="Settings" center sx={{ fontSize: "18px", position:"fixed", bottom:"0"}} {...a11yProps(2)}  />
+        </Tabs>
 
+        <TabPanel value={value} index={0}>
+          <ProjectOverview></ProjectOverview>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <GoalTracking></GoalTracking>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Calendar></Calendar>
+        </TabPanel>
+      </Box>
     </Box>
   );
 }
 
 export default Dashboard;
-
-
-
-
